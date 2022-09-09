6b1c284d7b32aa8043abc00baa2e1281ff0b78ff
AlexxTheCurator <alexanderatte@gmail.com>
2022-09-09 05:30:58 -0700

    Return value description

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,17 +2,14 @@
 #include <unistd.h>
 
 /**
- * main - main Entry
+ * main - Entry point
  *
- * write function, may double as printf alternative?
- *
- * main - prints text exactly
- *
- * Return value - error
+ * Return:Always 0
  */
 
 int main(void)
+
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
