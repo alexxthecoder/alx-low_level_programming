59f6ebf89a2bce5ac811b8c52abac53328651f9b
AlexxTheCurator <alexanderatte@gmail.com>
2022-09-09 03:06:22 -0700

    Betty style corrections

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
-
-/** main - prints exactly what was instructed
+/**
+ * main -main Entry
  *
- * Return - always 0
+ * write function, may double as printf alternative?
  *
+ * Return - Not 0
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
