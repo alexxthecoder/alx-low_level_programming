788aeb8923e71a7bab8ba0703162853370171554
AlexxTheCurator <alexanderatte@gmail.com>
2022-09-09 05:05:40 -0700

    More syntax corrections.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - main Entry
  *
  * write function, may double as printf alternative?
  *
- * main -prints text exactly
+ * main - prints text exactly
  *
  * Return value - error
  */
+
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
