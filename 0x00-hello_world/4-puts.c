9a0f50caa87cb9590e010a6a2696aded80ec185f
AlexxTheCurator <alexanderatte@gmail.com>
2022-09-08 18:42:40 -0700

    Even more bugs

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,17 +6,18 @@
  *
  * Written by - AlexxTheCurator
  *
- * Return - 0 (Success)
+ * Return - Always 0
  */
 
 int main(void)
 {
 
-char string[70] = "\"Programming is like building a multilingual puzzle\";
 
-puts(string);
+char string[70] = "\"Programming is like building a multilingual puzzle\"";
+
+	puts(string);
 
 
-puts("\"Programming is like building a multilingual puzzle\"");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle\"");
+	return (0);
 }
