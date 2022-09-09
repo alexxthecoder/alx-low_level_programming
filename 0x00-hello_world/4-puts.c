9cbdffb16c08b3e867811442ab6b8fd3db99a0b1
AlexxTheCurator <alexanderatte@gmail.com>
2022-09-08 18:20:13 -0700

    adjusted style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 
+
+/**
+ * main - entry
+ *
+ * Written by - AlexxTheCurator
+ *
+ * Return - Always 0
+ */
+
 int main(void)
 {
-	char string[70] = "\"Programming is like building a multilingual puzzle\"";
+
+	char string[70] = "\"Programming is like building a multilingual puzzle\";
+
 	puts(string);
+
 
 	puts("\"Programming is like building a multilingual puzzle\"");
 	return (0);
