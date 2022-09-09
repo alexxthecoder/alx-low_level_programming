7b6022042b82f1765a154d90f0a335ac63ed64f6
AlexxTheCurator <alexanderatte@gmail.com>
2022-09-08 17:34:21 -0700

    More research on the Betty style

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char string[70] = "\"Programming is like building a multilingual puzzle\"";
+	puts(string);
+
+	puts("\"Programming is like building a multilingual puzzle\"");
+	return (0);
+}
