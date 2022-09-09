783c228dd287e08bb4de7c1c3ee02daccdf0fda9
AlexxTheCurator <alexanderatte@gmail.com>
2022-09-09 02:24:18 -0700

    Still bug hunting

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main -main entry
+ * main -main Entry
  *
  * Return: 0
  */
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\"");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
