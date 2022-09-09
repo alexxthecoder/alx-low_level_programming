fa047b331b6c66a8e2f029ae1107df0a605a8e68
AlexxTheCurator <alexanderatte@gmail.com>
2022-09-08 18:58:48 -0700

    Adjusted for docu style again

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -18,6 +18,7 @@ char string[70] = "\"Programming is like building a multilingual puzzle\"";
 
 	puts(string);
 
+/* Checking out Betty stle docu, reult of main function is always zero */
 
 	puts("\"Programming is like building a multilingual puzzle\"");
 	return (0);
