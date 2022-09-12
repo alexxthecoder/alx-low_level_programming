f69ca271471e64db7bcf63eec9fb82cddb79d7d7
AlexxTheCurator <alexanderatte@gmail.com>
2022-09-12 09:35:59 -0700

    base 10 basically normal number base

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 0; ch < 10; ch++)
+	{
+		printf("%d", ch);
+	}
+	printf('\n');
+	return (0);
+}
