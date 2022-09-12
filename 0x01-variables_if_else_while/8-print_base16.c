35ccaf2c1a8f14103e9cd1a8349faf97a4a84dee
AlexxTheCurator <alexanderatte@gmail.com>
2022-09-12 10:33:35 -0700

    base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,13 +8,14 @@
 
 int main(void)
 {
-		int ch;
-		char dh;
+	int ch;
+	char dh;
 
 	for (ch = 48; ch < 58; ch++)
 		putchar(ch);
 	for (dh = 'a'; dh <= 'f'; dh++)
 		putchar(dh);
+
 	putchar('\n');
 	return (0);
 }
