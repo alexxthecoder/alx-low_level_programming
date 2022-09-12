689b891fe50917c8889131790cd230950386a77e
AlexxTheCurator <alexanderatte@gmail.com>
2022-09-12 10:14:06 -0700

    Base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+		int ch;
+		char dh;
+
+	for (ch = 48; ch < 58; ch++)
+		putchar(ch);
+	for (dh = 'a'; dh <= 'f'; dh++)
+		putchar(dh);
+	putchar('\n');
+	return (0);
+}
