abf95be95d5b652cddfd30b5006b8f039d505e28
AlexxTheCurator <alexanderatte@gmail.com>
2022-09-14 16:23:18 -0700

    Header files

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,21 @@
-#include <main.h>
+#include "main.h"
 /**
  * main - Entry
  *
- * Return: 0
+ * Return: void
  */
 
 int main(void)
 
 {
-	_putchar('_putchar\n');
+	_putchar('_\n');
+	_putchar('p\n');
+	_putchar('u\n');
+	_putchar('t\n');
+	_putchar('c\n');
+	_putchar('h\n');
+	_putchar('a\n');
+	_putchar('r\n');
+
 	return (0);
 }
